a444af44a790cfcfde9cc0b7c40b0dd1ca9ebe58
rajashadab17 <rajashadab1711@gmail.com>
2025-09-27 14:52:58 +0500

    perf(build): reduce compile time by adding tailwind.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.cache = {
+      type: "filesystem",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
+
